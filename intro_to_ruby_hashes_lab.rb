7eb5ff002e9eeca0088f55e714e4e05af3274dea
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-09-09 13:28:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,10 @@
-#def base_hash
-#  {}
-#end
-
-def monopoly 
-  monopoly = {railroads: {}}
+def base_hash
+ monopoly = {railroads: {}}
 end
+
+#def monopoly 
+#  monopoly = {railroads: {}}
+#end
 
 def monopoly_with_second_tier
   # When you start writing the implementation for this method, copy the Hash
